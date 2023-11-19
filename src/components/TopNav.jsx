51512075895b9661e81eaea4499932213f03d3b2
ruchitapaithankar15 <ruchitapaithankar@gmail.com>
2023-11-18 21:39:48 -0500

    login dropdown css changes

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -35,7 +35,7 @@ function TopNav() {
                     
                 </div>
 
-                <div id="user-dropdown" class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
+                <div id="user-dropdown" class="absolute top-50 right-10 z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
                         <div class="px-4 py-3">
                         <span class="block text-sm text-gray-900 dark:text-white">Username</span>
                         <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">userEmail@gmail.com</span>
