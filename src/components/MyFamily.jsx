0861f93d6320a0ba62e71b1a1c0a3c21a17faed1
ruchitapaithankar15 <ruchitapaithankar@gmail.com>
2023-11-18 19:14:00 -0500

    login downdown

--- src/components/MyFamily.jsx
+++ src/components/MyFamily.jsx
@@ -4,14 +4,6 @@ import TopNav from "./TopNav";
 // import SelectedFamily from './SelectedFamily'
 import RecentUploads from "./RecentUploads";
 import Rewards from "./Rewards";
-import React, { useState } from 'react'
-import useLoginCheck from './hooks/useLoginCheck'
-import BottomNav from './BottomNav'
-import TopNav from './TopNav'
-// import SelectedFamily from './SelectedFamily'
-import RecentUploads from './RecentUploads'
-import Rewards from './Rewards'
-import NotFamily from './NotFamily'
 
 // import this dependecies to use our hooks
 import useFetchFamilies from "./hooks/useFetchFamily";
@@ -160,31 +152,31 @@ function MyFamily() {
     </div>
   );
     //For testing purposes
-    const [isPartOfFamily, setIsPartOfFamily] = useState(true);
+//     const [isPartOfFamily, setIsPartOfFamily] = useState(true);
 
-    useLoginCheck({
-        redirect: "/Login"
-    });
+//     useLoginCheck({
+//         redirect: "/Login"
+//     });
 
-  return (<>
-    <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        onClick={() => {setIsPartOfFamily(!isPartOfFamily)}}>Is Part of Family Toggle For Testing</button>
-    {isPartOfFamily &&
-        <div>
-            <TopNav/>
-            <h1 className="flex justify-center items-center">.</h1>
-            <h1 className="mb-4 text-1xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white flex justify-center items-center">My Family</h1>
-            {/* <div className="flex justify-center items-center"><SelectedFamily/></div> */}
-            <div className="flex ml-10 mt-10  justify-left"><RecentUploads/></div>
+//   return (<>
+//     <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+//         onClick={() => {setIsPartOfFamily(!isPartOfFamily)}}>Is Part of Family Toggle For Testing</button>
+//     {isPartOfFamily &&
+//         <div>
+//             <TopNav/>
+//             <h1 className="flex justify-center items-center">.</h1>
+//             <h1 className="mb-4 text-1xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white flex justify-center items-center">My Family</h1>
+//             {/* <div className="flex justify-center items-center"><SelectedFamily/></div> */}
+//             <div className="flex ml-10 mt-10  justify-left"><RecentUploads/></div>
             
-            <Rewards/>
-            <BottomNav/>
-        </div>
-    }
-    {!isPartOfFamily &&
-        <NotFamily/>
-    }
-  </>)
+//             <Rewards/>
+//             <BottomNav/>
+//         </div>
+//     }
+//     {!isPartOfFamily &&
+//         <NotFamily/>
+//     }
+//   </>)
 }
 
 export default MyFamily;
