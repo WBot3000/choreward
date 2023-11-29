f84d1c94116f0e3a653c27753ddc471fd8d1d5fd
KKbeckang <beckang@bu.edu>
2023-11-29 16:49:04 -0500

    Updated Update and delete Function

--- src/components/hooks/useFetchChallenges.js
+++ src/components/hooks/useFetchChallenges.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { API, graphqlOperation } from 'aws-amplify';
-import { listChallenges } from '../../graphql/queries'; // Import the query
-import { createChallenge } from '../../graphql/mutations'; // Import the mutation
+import { listChallenges,getChallenges } from '../../graphql/queries'; // Import the query
+import { createChallenge,updateChallenges,deleteChallenges } from '../../graphql/mutations'; // Import the mutation
 
 const useFetchChallenges = () => {
   const [challenges, setChallenges] = useState([]);
@@ -23,12 +23,40 @@ const useFetchChallenges = () => {
       console.error('Error creating a challenge:', err);
     }
   };
+  const fetchChallengeById = async (id) => {
+    try {
+      const challengeData = await API.graphql(graphqlOperation(getChallenges, { id: id }));
+      return challengeData.data.getChallenge;
+    } catch (err) {
+      console.error('Error fetching challenge by ID:', err);
+    }
+  };
+  const updateChallengeById = async (id, newData) => {
+    try {
+      const currentChallengeData = await fetchChallengeById(id);
+      const updatedChallenge = { ...currentChallengeData, ...newData };
+      await API.graphql(graphqlOperation(updateChallenges, { input: updatedChallenge }));
+      fetchChallenges(); // Refresh the challenges list
+    } catch (err) {
+      console.error('Error updating challenge:', err);
+    }
+  };
+  const deleteChallengeById = async (id) => {
+    try {
+      await API.graphql(graphqlOperation(deleteChallenges, { input: { id } }));
+      fetchChallenges(); // Refresh the challenges list after deletion
+    } catch (err) {
+      console.error('Error deleting challenge:', err);
+    }
+  };
+
+  // ... (rest o
 
   useEffect(() => {
     fetchChallenges();
   }, []);
 
-  return { challenges, addChallenge };
+  return { challenges, addChallenge,updateChallengeById,fetchChallengeById,deleteChallengeById };
 };
 
 export default useFetchChallenges;
