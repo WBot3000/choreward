d638ba19af30c3be5e909a63d83ff2de8c789530
ruchitapaithankar15 <ruchitapaithankar@gmail.com>
2023-12-04 22:18:22 -0500

    Redeem Earned Points

--- src/components/RewardsDisplay.jsx
+++ src/components/RewardsDisplay.jsx
@@ -9,6 +9,7 @@ function RewardsDisplay() {
   const { families, fetchFamilies,fetchFamilyById } = useFetchFamilies();
   const [data, setData] = useState(null);
   const [Reward, setRewards] = useState("");
+  const [EarnedPoints, setEarnedPoints] = useState(null);
   const updatedItems = null;
   // const [UpdatedRewardList, setUpdatedRewardList] = useState();
   
@@ -21,6 +22,7 @@ function RewardsDisplay() {
         // console.log("fetchByID", result)
         // setData(result.getFamilies);
         setRewards(result.getFamilies.Rewards)
+        setEarnedPoints(result.getFamilies.EarnedPoints)
       } catch (error) {
         // Handle errors
         console.error('Error fetching data:', error);
@@ -31,27 +33,25 @@ function RewardsDisplay() {
   }, []);
 
     const {RewardName, RewardCost} = Reward
+    const PointsEarned = EarnedPoints
+    // console.log("These are the points",PointsEarned)
     // console.log("rewards HERE", RewardName, RewardCost)
 
     const items = [
         {
           text: 'Ice-cream',
           points:500,
-          viewButton: <button>Redeem</button>,
         },
         {
           text: 'Video Game',
           points:2500,
-          viewButton: <button>Redeem</button>,
         }
         ,
         {
           text: RewardName,
           points:RewardCost,
-          viewButton: <button>Redeem</button>,
         },
   ];
-
   const [UpdatedRewardList, setUpdatedRewardList] = useState(items);
   const [newReward, setNewReward] = useState({ RewardName: '', RewardCost: '' });
 
@@ -81,15 +81,6 @@ function RewardsDisplay() {
     }
   };
   console.log("THESE ARE UPDATED reward list", UpdatedRewardList)
-    // const [modalOpen, setModalOpen] = useState(false);
-  
-    // const openModal = () => {
-    //   setModalOpen(true);
-    // };
-  
-    // const closeModal = () => {
-    //   setModalOpen(false);
-    // };
 
 
     let [isOpen, setIsOpen] = useState(false)
@@ -104,21 +95,16 @@ function RewardsDisplay() {
     setIsOpen(true)
   }
 
-
+  const handleRedeem = (points) => {
+    const updatedPoints = parseInt(EarnedPoints) + points; // Update earned points
+    setEarnedPoints(updatedPoints);
+  };
   return (
     <div>
-      <div className="flex">
-      <h2 className="mb-2 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl dark:text-white ml-10 mt-10">Rewards</h2>
-      {/* <link></link>
-      <button onClick={openModal}>Add Rewards</button>
-      <button className="ml-10" onClick={closeModal}> X </button>
-      </div>
-      {modalOpen && (
-      <div className="modal">
-          <div className="modal-content">
-          <RewardsModal/>
-          </div>
-        </div>)} */}
+      {/* <button type="button" class="ml-10 mt-10 mb-2 text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-10 py-5 text-center me-2 mb-2">Earned Points {parseInt(EarnedPoints)+1000}</button> */}
+      <button type="button" class="ml-10 mt-10 mb-2 text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-10 py-5 text-center me-2 mb-2">Earned Points {parseInt(EarnedPoints)}</button>
+      <div class="flex">
+      <h2 class="mb-2 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl dark:text-white ml-10 mt-10">Rewards</h2>
       </div>
 
 
@@ -128,12 +114,12 @@ function RewardsDisplay() {
     <div className="flex pb-10">
     {UpdatedRewardList.map((item,index) => (
     <article key={index}>
-           <div className="flex justify-center items-center pt-10 pl-10">
- <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-     <div className="flex flex-col justify-between p-4 leading-normal">
-         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.text}</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{item.points} points</p>
-        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Redeem</button>
+           <div class="flex justify-center items-center pt-10 pl-10">
+ <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+     <div class="flex flex-col justify-between p-4 leading-normal">
+         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.text}</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{item.points} points</p>
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={() => handleRedeem(item.points)}>Redeem</button>
      </div>
     </a>
     </div>
@@ -152,7 +138,7 @@ function RewardsDisplay() {
 
 
 <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Dialog as="div" class="relative z-10" onClose={closeModal}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -162,11 +148,11 @@ function RewardsDisplay() {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-black/25" />
+            <div class="fixed inset-0 bg-black/25" />
           </Transition.Child>
 
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <div class="fixed inset-0 overflow-y-auto">
+            <div class="flex min-h-full items-center justify-center p-4 text-center">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
@@ -176,10 +162,10 @@ function RewardsDisplay() {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
+                    class="text-lg font-medium leading-6 text-gray-900"
                   >
                     Add Rewards
                   </Dialog.Title>
@@ -197,7 +183,7 @@ function RewardsDisplay() {
                   <div className="mt-4">
                     <button
                       type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       onClick={handleAddReward} 
                     >
                       Add
